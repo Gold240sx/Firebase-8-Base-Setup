9715e2dc7c0feccc7548806247d05a1a4ae120f3
Gold240sx <240designworks@gmail.com>
2022-05-03 14:35:51 -0500

    Initial Commit - Base Form, Modal, Input, and Firebase setup with materialize.

--- JS/FIREBASE/firebase.js
+++ JS/FIREBASE/firebase.js
@@ -1,10 +1,15 @@
 var config = {
-    //... PASTE CONFIG FILE HERE ...
+    apiKey: "AIzaSyC-l9VdSKzYww9hZ7soSUffIJxwT2aLA3I",
+    authDomain: "d3-database-21abf.firebaseapp.com",
+    projectId: "d3-database-21abf",
+    storageBucket: "d3-database-21abf.appspot.com",
+    messagingSenderId: "564128085624",
+    appId: "1:564128085624:web:0a4ee7701aa64358351f8d"
 };
 firebase.initializeApp(config);
 const db = firebase.firestore();
 const settings = {timestampsInSnapshots: true, merge: true};
 db.settings(settings)
 
-console.log(db)
+//console.log(db)
 export { db }
--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 // imports
-import './JS/FIREBASE/firebase.js'
-import './JS/data.js';
+//import './JS/FIREBASE/firebase.js'
+// import { data } from './JS/data.js';
 import './JS/graph.js';
 
+import { db } from "./JS/FIREBASE/firebase.js"
 
+const modal = document.querySelector('.modal')
+//materialize function for activating the modal-trigger
+M.Modal.init(modal)
+
+const form = document.querySelector('form')
+const name = document.querySelector('#name')
+const parent = document.querySelector('#parent')
+const department = document.querySelector('#department')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    console.log(db)
+    db.collection('employees').add({
+        name: name.value, 
+        parent: parent.value, 
+        department: department.value
+    })
+    var instance = M.Modal.getInstance(modal)
+    instance.close();
+
+    form.reset()
+})
