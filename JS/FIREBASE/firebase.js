92009723cc2bfc5bcc4dd60c3f72af88a6ddf170
Gold240sx <240designworks@gmail.com>
2022-05-03 14:37:26 -0500

    initial commit

--- JS/FIREBASE/firebase.js
+++ JS/FIREBASE/firebase.js
@@ -1,10 +1,5 @@
 var config = {
-    apiKey: "AIzaSyC-l9VdSKzYww9hZ7soSUffIJxwT2aLA3I",
-    authDomain: "d3-database-21abf.firebaseapp.com",
-    projectId: "d3-database-21abf",
-    storageBucket: "d3-database-21abf.appspot.com",
-    messagingSenderId: "564128085624",
-    appId: "1:564128085624:web:0a4ee7701aa64358351f8d"
+    // ** Paste your Firebase Config Here**
 };
 firebase.initializeApp(config);
 const db = firebase.firestore();
